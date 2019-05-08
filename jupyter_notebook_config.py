c94d173b937221ff544a8db74d681595c48bab5f
kongroo <imjcqt@gmail.com>
2019-05-08 21:01:39 +0800

    modify notebook dir

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -2,5 +2,5 @@ c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.open_browser = False
 c.NotebookApp.port = 8888
 # c.NotebookApp.base_url = '/ipython/'
-# c.NotebookApp.notebook_dir = './notebook/'
+c.NotebookApp.notebook_dir = '/root'
 
